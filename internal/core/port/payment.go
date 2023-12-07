1e5c4853fda7a23ec02bbbbc01dc6c63e40b2a03
Domains18 <gibsonsgibson88@gmail.com>
2023-12-07 23:43:18 +0300

    port  functions

--- internal/core/port/payment.go
+++ internal/core/port/payment.go
@@ -8,4 +8,14 @@ import (
 type PaymentRepository interface {
 	CreatePayment(ctx context.Context, payment *domain.Payment) (*domain.Payment, error)
 	GetPaymentById(ctx context.Context, id uint64) (*domain.Payment, error)
+	ListPayment(ctx context.Context, skip, limit uint64) (*domain.Payment, error)
+	UpdatePayment(ctx context.Context, payment *domain.Payment) (*domain.Payment, error)
+	DeletePayment(ctx context.Context, id uint64) error
+}
+
+type PaymentService interface {
+	CreatePayment(ctx context.Context, payment *domain.Payment) (*domain.Payment, error)
+	GetPayment(ctx context.Context, id uint64) (*domain.Payment, error)
+	ListPayments(ctx context.Context, skip, limit uint64)
+	UpdatePayment(ctx context.Context, id uint64) error
 }
--- internal/core/port/product.go
+++ internal/core/port/product.go
@@ -1,1 +1,15 @@
 package port
+
+import (
+	"context"
+	"github.com/Domains18/go-pos/internal/core/domain"
+)
+type err *error
+
+type ProductRepository interface {
+	createProduct(ctx context.Context, product *domain.Product)(*domain.Product, error)
+	GetProduct(ctx context.Context, id uint64)(*domain.Product, err)
+	ListProducts(ctx context.Context, search string, categoryId, skip, limit uint64)([]domain.Product, err)
+	UpdateProduct(ctx context.Context, product *domain.Product)(*domain.Product, err)
+	DeleteProduct(ctx context.Context, id uint64) err
+}
