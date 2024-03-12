c473570ae08f4b70f814a7d95f682af60441481f
shadmeoli <shadrack@shopzetu.com>
2024-03-12 17:25:33 +0300

    update: templ clean up

--- internal/server/controller/index.go
+++ internal/server/controller/index.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 )
 
-func HomeRoute(c *gin.Context) {
-	c.JSON(200, gin.H{"hello": "world"})
+func HomeRoute(c *gin.Context, component templ.Component) error {
+	c.Status(200)
+	return component.Render(c.Request.Context(), c.Writer)
 }
--- internal/server/index.go
+++ internal/server/index.go
@@ -1,12 +1,16 @@
 package server
 
 import (
+
+	// "github.com/Nerds-Catapult/notiwa/view"
 	"github.com/Nerds-Catapult/notiwa/internal/server/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/", controller.HomeRoute)
+	router.GET("/", func(c *gin.Context) {
+		controller.HomeRoute(c, view.Home.HomePage)
+	})
 	return router
 }
