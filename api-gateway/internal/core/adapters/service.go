4df4086877c4bb60bd594f388316f1643153b622
MikeMwita <mwitamike8@gmail.com>
2024-03-17 15:21:57 +0300

    Define API contract

--- api-gateway/internal/core/adapters/service.go
+++ api-gateway/internal/core/adapters/service.go
@@ -2,10 +2,17 @@ package adapters
 
 import (
 	"context"
-	"github.com/Nerds-Catapult/notiwa/internal/dto"
+	"github.com/Nerds-Catapult/notiwa/api-gateway/internal/dto"
+	"net/http"
 )
-
 type AuthRepo interface {
-	Login(ctx context.Context,username, password string) (*dto.Token, error)
+	Login(ctx context.Context, username, password string) (*dto.Token, error)
+}
 
+type Server interface {
+	CreateCustomers(ctx context.Context, body dto.PostCustomersJSONRequestBody) (*http.Response, error)
+	GetCustomers(ctx context.Context, params *dto.GetCustomersParams) (*http.Response, error)
+	LoginCus(ctx context.Context, body dto.PostLoginJSONRequestBody) (*http.Response, error)
+	GetProducts(ctx context.Context, params *dto.GetProductsParams) (*http.Response, error)
+	CreateSalesInvoices(ctx context.Context, body dto.PostSalesInvoicesJSONRequestBody) (*http.Response, error)
 }
