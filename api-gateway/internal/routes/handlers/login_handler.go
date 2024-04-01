526a2a453cd22f267bb35be9a061731dfaf0daa9
MikeMwita <mwitamike8@gmail.com>
2024-04-01 16:03:32 +0300

    Add login handler

--- api-gateway/internal/routes/handlers/login_handler.go
+++ api-gateway/internal/routes/handlers/login_handler.go
@@ -2,43 +2,34 @@ package handlers
 
 import (
 	"github.com/Nerds-Catapult/notiwa/api-gateway/internal/core/adapters"
-	"github.com/Nerds-Catapult/notiwa/api-gateway/internal/dto"
-	"net/http"
 )
 
 type AuthHandler struct {
 	AuthRepo adapters.AuthRepo
 }
 
-func (h *AuthHandler) CreateCustomers(ctx context.Context, body dto.PostCustomersJSONRequestBody) (*http.Response, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *AuthHandler) GetCustomers(ctx context.Context, params *dto.GetCustomersParams) (*http.Response, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *AuthHandler) LoginCustomer(ctx context.Context, body dto.PostLoginJSONRequestBody) (*http.Response, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *AuthHandler) GetProducts(ctx context.Context, params *dto.GetProductsParams) (*http.Response, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *AuthHandler) CreateSalesInvoices(ctx context.Context, body dto.PostSalesInvoicesJSONRequestBody) (*http.Response, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-
-
-func NewAuthHandler(authRepo adapters.AuthRepo) *AuthHandler {
-	return &AuthHandler{
-		AuthRepo: authRepo,
-	}
-}
+//
+//func (h *AuthHandler) Login(c *gin.Context) {
+//	var creds dto.Credentials
+//	if err := c.ShouldBindJSON(&creds); err != nil {
+//		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+//		return
+//	}
+//
+//	ctx := c.Request.Context()
+//
+//	token, err := h.AuthRepo.Login(ctx, creds.Username, creds.Password)
+//	if err != nil {
+//		c.JSON(http.StatusForbidden, gin.H{"error": "Invalid username or password"})
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, token)
+//}
+//
+//
+//func NewAuthHandler(authRepo adapters.AuthRepo) *AuthHandler {
+//	return &AuthHandler{
+//		AuthRepo: authRepo,
+//	}
+//}
