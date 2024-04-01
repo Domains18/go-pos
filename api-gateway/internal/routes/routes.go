7da1a48671e7317d91cfefcce0aa7660486ddc10
MikeMwita <mwitamike8@gmail.com>
2024-04-01 16:07:07 +0300

    Setup project routes

--- api-gateway/internal/routes/routes.go
+++ api-gateway/internal/routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/Nerds-Catapult/notiwa/api-gateway/internal/routes/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(router *gin.Engine, handler *handlers.Handler) {
+	router.POST("/login", handlers.LoginCustomer(handler))
+	router.POST("/customers", handler.CreateCustomers)
+	router.GET("/customers", handler.GetCustomers)
+	router.POST("/sales-invoices", handler.CreateSalesInvoices)
+	router.GET("/products", handler.GetProducts)
+}
